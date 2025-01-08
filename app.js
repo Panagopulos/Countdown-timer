f356e308e4da7e444ccec167f333c0cf6d97ada9
Panagopulos <tpanagopulos2@gmail.com>
2025-01-08 19:50:15 +0100

    Logic for  when giveaway ends + displaying it in DOM

---
+++ app.js
@@ -0,0 +1,50 @@
+const months = [
+    "January",
+    "Februrary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+const weekdays = [
+    "Sunday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2025, 2, 25, 11, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
+
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+const today = new Date().getTime();
+const t = futureTime - today;
+}
+
+getRemainingTime();
