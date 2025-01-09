49215edc54ed751b6a68bc0d3108ac2386954d12
Panagopulos <tpanagopulos2@gmail.com>
2025-01-09 14:59:50 +0100

    counting the rest of the time. Create array for the time. use function to format it. Foreach for iteration.

--- app.js
+++ app.js
@@ -53,7 +53,30 @@ const oneMinute = 60*1000;
 
 let days = t/oneDay;
 days = Math.floor(days);
-let hours = t / oneHour;
+let hours = Math.floor((t % oneDay) / oneHour);
+let minutes = Math.floor((t % oneHour) / oneMinute);
+let seconds = Math.floor((t % oneMinute) / 1000);
+
+//
+const values = [days,hours,minutes,seconds];
+
+function format(item) {
+    if (item < 10) {
+        return item = `0${item}`;
+    }
+    return item;
 }
+
+items.forEach((item,index) => {
+    item.innerHTML = format(values[index]);
+});
+if(t<0){
+    clearInterval(countdown)
+    deadline.innerHTML = `<h4 class="expired>sorry, this giveaway has expired</h4>"`;
+}
+}
+
+//countdown
+let countdown = setInterval(getRemainingTime,1000);
 
 getRemainingTime();
